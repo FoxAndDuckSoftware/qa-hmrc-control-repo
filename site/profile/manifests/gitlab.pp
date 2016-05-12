d94c5803af191ee817f555c4d4fc3ba810096e1f
Martin Kemp <martin@martinke.mp>
2016-05-12 11:25:42 +0100

    Prefix gitlab url with http

--- site/profile/manifests/gitlab.pp
+++ site/profile/manifests/gitlab.pp
@@ -3,6 +3,6 @@
 # Defines what is required for the gitlab instance.
 class profile::gitlab( $externalurl = $::servername ) {
     class { '::gitlab':
-        external_url => $externalurl
+        external_url => join('http://', $externalurl)
     }
 }
