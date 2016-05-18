c94637f020801e6b62a9b92bcea592567166d24a
Martin Kemp <martin@martinke.mp>
2016-05-18 12:54:16 +0100

    Add nexus profile and role

---
+++ site/profile/manifests/nexus.pp
@@ -0,0 +1,19 @@
+# == Class: profile::nexus
+#
+class profile::nexus {
+    class { '::nexus':
+        version    => '2.13.0',
+        revision   => '01',
+        nexus_root => '/opt'
+    }
+    class { '::nginx': }
+    nginx::resource::vhost { 'nexus':
+        ensure => present
+    }
+    nginx::resource::location { 'nexus':
+        ensure   => present,
+        location => '/',
+        vhost    => 'nexus',
+        proxy    => "http://${::nexus::nexus_host}:${::nexus::nexus_port}"
+    }
+}
