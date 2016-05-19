4a91e2d6a0f44b9bfda76d4fef47459e96754a65
James Smith <James Smith>
2016-05-19 15:03:20 +0100

    Fixed template declarations in logstash profile,  Also added in parameters

--- site/profile/manifests/elasticsearch/logstash.pp
+++ site/profile/manifests/elasticsearch/logstash.pp
@@ -1,7 +1,16 @@
 # == Class: profile::elasticsearch::logstash
 #
 # Defines what is required for the logstash instance. 
-class profile::elasticsearch::logstash {
+class profile::elasticsearch::logstash (Numeric $filebeat_port = 5044,
+										Boolean $filebeat_ssl_enabled = false,
+										Array $filebeat_ssl_certificate_authorities,
+										String $filebeat_certificate,
+										String $filebeat_ssl_key,
+										String $filebeat_ssl_verify_mode,
+										Array $elasticsearch_hosts = ["localhost:9200"],
+										Boolean $elasticsearch_ssl_enabled = false,
+										Array $elasticsearch_cacert,
+										) {
     class { 'logstash' :
 		manage_repo  => true,
 		java_install => false,
@@ -13,8 +22,13 @@ class profile::elasticsearch::logstash {
 		order   => 10,
 	}
 #Inputs from remote hosts will be transported using beats
-	logstash::configfile { 'input_beats' () :
-		template => 'puppet:///site/profile/manifests/templates/input_beats.epp',
+	logstash::configfile { 'input_beats' :
+		content => template('puppet:///site/profile/manifests/templates/input_beats.epp', { 'filebeat_port'               => $filebeat_port, 
+																							'ssl_enabled'                 => $filebeat_ssl_enabled, 
+																							'ssl_certificate_authorities' => $filebeat_ssl_certificate_authorities,
+																							'certificate'                 => $filebeat_certificate,
+																							'ssl_key'                     => $filebeat_ssl_key,
+																							'ssl_verify_mode              => $filebeat_ssl_verify_mode}),
 		order    => 11,
 	}
 # Defines the Config files for the filters for the various inputs
@@ -35,12 +49,14 @@ class profile::elasticsearch::logstash {
 #	}
 # Defines the config file used for the output to elasticsearch
 	logstash::configfile { 'output_es' ():
-		template => 'puppet:///site/profile/manifests/templates/output_es.epp'
+		content  => template('puppet:///site/profile/manifests/templates/output_es.epp', { 'elasticsearch_hosts' => $elasticsearch_hosts,
+																						   'ssl_enabled' => $elasticsearch_ssl_enabled,
+																						   'elasticsearch_cacert' => $elasticsearch_cacert}),
 		order    => 30,
 	}
 # Defines the source for custom grok patterns used by the filters.
 	logstash::patternfile { 'extra_patterns' :
 		source => 'puppet:///site/profile/manifests/files/extra_patterns',
 	}
-	
+
 }
