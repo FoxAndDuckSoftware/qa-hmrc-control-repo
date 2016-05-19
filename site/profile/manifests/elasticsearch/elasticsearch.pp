ae9fc5d7e0d54b4feb4eeb257ff4d58970459074
James Smith <James Smith>
2016-05-19 14:16:19 +0100

    Initial Elasticsearch config

---
+++ site/profile/manifests/elasticsearch/elasticsearch.pp
@@ -0,0 +1,32 @@
+# == Class: profile::elasticsearch::elasticsearch
+#
+# Defines what is required for the elasticsearch instance.
+
+class profile::elasticsearch::elasticsearch ( Boolean $node_master = true,
+											  Boolean $node_data = true,
+											  Numeric $minimum_masterNodes = 3
+											  Numeric $http_port = 9200,
+											) { 
+
+	class { 'elasticsearch' : 
+		config           => {
+			'node'       => {
+				'master' => $node_master,
+				'data'   => $node_data,
+				}
+			'discovery'  => {
+				'zen.minimum_master_nodes' => $minimum_masterNodes,
+				}
+			'http'       => {
+				'port'   => $http_port,
+				}
+			}
+		}
+
+# Customisation of the instance is done in the below section
+	elasticsearch::instance { 'instancename' :
+		
+	}
+}
+
+  
