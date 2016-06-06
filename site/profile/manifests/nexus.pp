28531c09a2e9a65cc2b72c5bac99ef2179431878
Martin Kemp <martin@martinke.mp>
2016-06-06 12:01:19 +0100

    Change nexus root directory

--- site/profile/manifests/nexus.pp
+++ site/profile/manifests/nexus.pp
@@ -4,7 +4,7 @@ class profile::nexus {
     class { '::nexus':
         version    => '2.13.0',
         revision   => '01',
-        nexus_root => '/opt'
+        nexus_root => '/opt/nexus'
     }
     class { '::nginx': }
     nginx::resource::vhost { '_':
