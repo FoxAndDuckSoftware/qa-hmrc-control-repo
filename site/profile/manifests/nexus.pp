93fc8c092b71f9bdaaa243c31c2abd70ec04b442
Martin Kemp <martin@martinke.mp>
2016-06-06 12:15:02 +0100

    Ensure nexus dir exists and is owned by nexus

--- site/profile/manifests/nexus.pp
+++ site/profile/manifests/nexus.pp
@@ -1,14 +1,20 @@
 # == Class: profile::nexus
 #
 class profile::nexus {
-    class { '::nexus':
-        version    => '2.13.0',
-        revision   => '01',
-        nexus_root => '/opt/nexus'
-    }
-    class { '::nginx': }
-    nginx::resource::vhost { '_':
-        ensure => present,
-        proxy  => "http://${::nexus::nexus_host}:${::nexus::nexus_port}"
-    }
+
+  file { '/opt/nexus':
+    ensure => directory,
+    owner  => 'nexus',
+    group  => 'nexus'
+  } ->
+  class { '::nexus':
+      version    => '2.13.0',
+      revision   => '01',
+      nexus_root => '/opt/nexus'
+  }
+  class { '::nginx': }
+  nginx::resource::vhost { '_':
+      ensure => present,
+      proxy  => "http://${::nexus::nexus_host}:${::nexus::nexus_port}"
+  }
 }
